9bc5398b5ecea3f4057a8dffc6ebd9d00baa1e55
Ale Muñoz <bomberstudios@gmail.com>
2015-11-24 17:39:44 +0100

    Lunr proof-of-concept

--- js/main.js
+++ js/main.js
@@ -44,3 +44,50 @@ $(window).resize(function(){
   //   });
   // }
 });
+
+$(document).ready(function(){
+  var documentCollection,
+      lunrIndex = lunr(function () {
+        this.field('title', {boost: 10})
+        this.field('body')
+        this.field('url')
+        this.ref('id')
+      }),
+      url = '/js/lunr.json'
+
+  $.ajax({
+    url: url,
+    crossDomain: true,
+    success: function(data,status,xhr){
+      documentCollection = data
+      $.each(data,function(index, value){
+        lunrIndex.add(value)
+      })
+      search_enable()
+    },
+    error: function(xhr, type){
+      console.log("Error type: " + type)
+      console.log(xhr)
+    }
+  })
+
+  function search_enable() {
+    $('.search_form').show()
+    $('.search_form #search_field').on('keyup', function(){
+      var searchTerm = $(this).val()
+      if (searchTerm.length > 3) {
+        search(searchTerm)
+      }
+    })
+  }
+  function search(text){
+    var results = lunrIndex.search(text),
+        htmlOutput = "<ul>"
+    $.each(results, function(index, result){
+      var searchResult = documentCollection[result.ref - 1] // Because Lunr results are 0-based, and Jekyll collections are 1-based
+      htmlOutput += "<li><a href='" + searchResult.url + "'>" + searchResult.title + "</a></li>"
+    })
+    htmlOutput += "</ul>"
+    $('.search_results').html(htmlOutput)
+  }
+})
