bef65bf672093a203a1d32ea04460fb2709187a3
Alberto Calvo <intemperie@gmail.com>
2015-11-30 15:06:49 +0100

    Moving to single quotes

--- js/main.js
+++ js/main.js
@@ -1,56 +1,48 @@
 // Main navigation logic
-$(".main-nav .nav-toggle").click(function(){
-  $(this).toggleClass("active");
-  $(".main-nav ul").toggleClass("active");
+$('.main-nav .nav-toggle').click(function(){
+  $(this).toggleClass('active');
+  $('.main-nav ul').toggleClass('active');
 
   if ($('body').attr('style')) {
       $('body').removeAttr('style');
     } else {
-      $('body').attr("style","overflow:hidden;"); //Avoids scrolling of the underlying content TODO fix on Safari Mobile
+      $('body').attr('style","overflow:hidden;'); //Avoids scrolling of the underlying content TODO fix on Safari Mobile
     }
 
-  $(".overlay").toggleClass("active");
+  $('.overlay').toggleClass('active');
 });
 
 // TOC navigation logic
-$(".toc-nav .nav-toggle").click(function(){
+$('.toc-nav .nav-toggle').click(function(){
   if (window.innerWidth > 767) { return }
-  $(this).toggleClass("active");
-  $(".toc-nav ol").toggle();
+  $(this).toggleClass('active');
+  $('.toc-nav ol').toggle();
 });
 
 // Overlay logic (dismiss everything when you click it)
-$(".overlay").click(function(){
-  $(".main-nav .nav-toggle").toggleClass("active");
-  $(".main-nav ul").toggleClass("active");
+$('.overlay').click(function(){
+  $('.main-nav .nav-toggle').toggleClass('active');
+  $('.main-nav ul').toggleClass('active');
   $('body').removeAttr('style');
-  $(".overlay").removeClass("active all");
+  $('.overlay').removeClass('active all');
   $('.search-form').hide();
 });
 
 // Window resize changes needed
 $(window).resize(function(){
   if(window.innerWidth > 767) {
-    $(".main-nav ul").removeClass("active");
-    $(".nav-toggle").removeClass("active");
-    $("body").removeAttr("style");
-    $(".overlay").removeClass("active all");
-    $(".toc-nav ol").removeAttr("style");
+    $('.main-nav ul').removeClass('active');
+    $('.nav-toggle').removeClass('active');
+    $('body').removeAttr('style');
+    $('.overlay').removeClass('active all');
+    $('.toc-nav ol').removeAttr('style');
     $('.search-form').hide();
   }
-
-  //TODO make this work
-  // if(window.innerWidth < 767) {
-  //   $(".toc-nav .nav-toggle").click(function(){
-  //     $(this).toggleClass("active");
-  //     $(".toc-nav ol").toggle();
-  //   });
-  // }
 });
 
 // Search logic
-$(".search-toggle").click(function(){
-  $(".overlay").toggleClass("active all");
+$('.search-toggle').click(function(){
+  $('.overlay').toggleClass('active all');
   $('.search-form').show();
   $( ".search-field" ).focus();
 });
@@ -76,7 +68,7 @@ $(document).ready(function(){
       search_enable()
     },
     error: function(xhr, type){
-      console.log("Error type: " + type)
+      console.log('Error type: ' + type)
       console.log(xhr)
     }
   })
@@ -91,12 +83,12 @@ $(document).ready(function(){
   }
   function search(text){
     var results = lunrIndex.search(text),
-        htmlOutput = "<ul>"
+        htmlOutput = '<ul>'
     $.each(results, function(index, result){
       var searchResult = documentCollection[result.ref - 1] // Because Lunr results are 0-based, and Jekyll collections are 1-based
-      htmlOutput += "<li><a href='" + searchResult.url + "'>" + searchResult.title + "</a></li>"
+      htmlOutput += '<li><a href='" + searchResult.url + "'>' + searchResult.title + '</a></li>'
     })
-    htmlOutput += "</ul>"
+    htmlOutput += '</ul>'
     $('.search-results').html(htmlOutput)
   }
 })
