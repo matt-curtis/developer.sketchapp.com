584f94d18e65a8bb3a69401c7b6a213e20f3c59f
Alberto Calvo <intemperie@gmail.com>
2015-12-01 15:19:55 +0100

    Removed BS notification

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ gulp.task('build', shell.task(['jekyll build --watch']));
 
 // Task for serving site with Browsersync
 gulp.task('serve', function () {
-  browserSync.init({server: {baseDir: '_site/'}});
+  browserSync.init({
+    server: {baseDir: '_site/'},
+    notify: false
+  });
   // Reloads page when some of the already built files changed:
   gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
