c37830f8d185fcb04fc069f3132e00e26e5a2e25
Ale Muñoz <bomberstudios@gmail.com>
2015-12-09 11:26:44 +0100

    CSS injection is working. One step closer to #7187 :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync').create();
+var browserSync  = require('browser-sync').create()
+var exec         = require('child_process').exec
+var gulp         = require('gulp')
+var shell        = require('gulp-shell')
 
-// Task for building site when something changed:
-gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task('build', shell.task(['jekyll build --incremental']) )
 
-// Task for serving site with Browsersync
-gulp.task('serve', function () {
+gulp.task('watch', ['build'], function() {
+  exec('jekyll build --incremental --watch', function(err, stdout, stderr) {
+    if (err) {
+      console.log(err, stdout, stderr)
+    }
+  })
+})
+
+gulp.task('serve', ['watch'], function() {
   browserSync.init({
-    server: {baseDir: '_site/'},
-    notify: false
-  });
-  // Reloads page when some of the already built files changed:
-  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-});
+    server: "./_site",
+    files: './_site/**/*.*',
+    notify: false,
+    logPrefix: 'developer.sketchapp.com',
+    // Uncomment the next two lines to create a tunnel to https://developersketchapp.localtunnel.me
+    // tunnel: 'developersketchapp',
+    // online: true, // Set this to 'false' when working with no connection
+    open: 'external', // Can use 'local', 'external' or 'tunnel' (last one opens https://developersketchapp.localtunnel.me)
+    // Uncomment the next line if you want Gulp to open multiple browsers when running
+    // browser: ['google chrome', 'safari', 'firefox']
+  })
+})
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['serve'])
